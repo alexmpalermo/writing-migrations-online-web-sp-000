a00b45deab6c885f27cecd88c715b2bc17659506
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-01 18:36:43 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,10 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change 
-     change_column(:students, :birthdate, t.datetime)  do |t|
-      t.integer :grade
-      t.string :birthdate
-  end 
+     change_column(:students, :birthdate, t.datetime) 
 end
   
   
